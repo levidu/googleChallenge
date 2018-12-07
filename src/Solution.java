bbaf632b5f23c25a867142bab520104dc1155cd7
Levidu <levidualahakoon@gmail.com>
2018-12-07 17:14:18 -0500

    all set

--- src/Solution.java
+++ src/Solution.java
@@ -1,48 +1,53 @@
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class Solution {
-
-	public int solution(String[] L) {
-		
-		ArrayList<String>myList1 = new ArrayList<String>();
-
-
-		
-		for(String email: L) {
-
-			String[] atSplit = email.split("@");
-
-			String localName = atSplit[0];
-			String domainName = atSplit[1];
-
-			localName = localName.replaceAll("\\.", "");
-
-			localName = localName.split("\\+")[0];
-
-			String formatEmail = localName+"@"+domainName;
-
-			myList1.add(formatEmail);
-
-		}
-		
-		int count = 0; 
-		for(String a : myList1) {
-			
-			
-			
-			String localNameSnip = a.split("@")[0];
-			
-			if(localNameSnip.length() == 2) {
-				
-				count ++;
-			}
-				
-		}
-		
-		
-	
-		return count;
-
-	}
-
+    
+    public int solution(String[] L) {
+        
+        HashMap<String,Integer> lm = new HashMap<>();
+        
+        
+        
+        for(String email: L) {
+            
+            String[] atSplit = email.split("@");
+            
+            String localName = atSplit[0];
+            String domainName = atSplit[1];
+            
+            localName = localName.replaceAll("\\.", "");
+            
+            localName = localName.split("\\+")[0];
+            
+            String formatEmail = localName+"@"+domainName;
+            
+            
+            if(lm.containsKey(formatEmail)) {
+                
+                int keyVal = lm.get(formatEmail);
+                
+                lm.put(formatEmail, keyVal+1);
+            }
+            else {
+                
+                lm.put(formatEmail, 1);
+                
+            }
+        }
+        
+        int count = 0;
+        for(Integer i : (Collection<Integer> )lm.values()) {
+            
+            if(i > 1) {
+                
+                count ++;
+            }
+            
+        }
+        
+        return count;
+        
+    }
+    
 }
